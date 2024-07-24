0ee8e8adce3e24588feae06cf2ef26e0db7adc33
gabrielopes7 <gabrielplopes7@hotmail.com>
2024-07-23 22:19:48 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persistencia.Models;
+using EasyOrderAPI.ViewModel;
+using Microsoft.AspNetCore.Cors;
+
+namespace EasyOrderAPI.Controllers
+{
+    [ApiController]
+    [EnableCors]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository ?? throw new ArgumentNullException();
+        }
+
+        [HttpPost]
+        [Route("api/registrarUsuario")]
+        public IActionResult RegistrarUsuario(UsuarioViewModel usuarioView)
+        {
+            var usuario = new Usuario(usuarioView.Nome, usuarioView.Email, usuarioView.DataNascimento, usuarioView.Senha);
+            _usuarioRepository.Add(usuario);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("api/pegarListaUsuario")]
+        public IActionResult Get()
+        {
+            var usuario = _usuarioRepository.Get();
+
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Persistencia.Models;
+using Persistencia.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
